73402fb8816cc8eae24db138fb829c7371ac56da
Miller Ostos <millerandresoc@gmail.com>
2024-08-06 14:05:12 -0500

    add test

--- VisualizationSelector.test.js
+++ VisualizationSelector.test.js
@@ -1,11 +1,21 @@
-import VisualizationSelector from "./VisualizationSelector";
+import VisualizationSelector from "../VisualizationSelector";
 import { render, screen } from "@testing-library/react";
 
+const currentVisualization = 'alluvial';
+const type = 'country';
 
+const set = () => {
+
+}
 
 describe('Visualization selector', ()=> {
     test('initial render', () => {
-        render(<VisualizationSelector />);
+        render(<VisualizationSelector setCurrentVisualization={set} currentVisualization={currentVisualization} type={type} />);
         screen.debug();
+    });
+
+    test('should be show button for exante', () => {
+        render(<VisualizationSelector setCurrentVisualization={set} currentVisualization={currentVisualization} type={type} />);
+        screen.getByRole('button', { name: 'Ex-Ante' });
     });
 });
